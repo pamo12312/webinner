b5981e4e81754331eab7213e1da900cc1a7a55fa
Patrik <patri.moravec@email.cz>
2023-12-08 16:59:48 +0100

    přidání a mazání elemntu v js a přidání stylu v css

--- js.js
+++ js.js
@@ -12,3 +12,42 @@ function displayTextContent() {
 }
 displayInnerText();
 displayTextContent();
+
+
+const container = document.getElementById("container");
+const button = document.getElementById("myButton");
+
+function funkcePoKliknuti() {
+    // Vytvoření nového div elementu
+    const novyElement = document.createElement("div");
+
+    // Nastavení náhodné barvy pozadí
+    const nahodnaBarva = getRandomColor();
+    novyElement.style.backgroundColor = nahodnaBarva;
+
+    // Přidání textu
+    novyElement.textContent = "Nový Element 🌈";
+
+    // Přidání nového elementu do "container"
+    container.appendChild(novyElement);
+
+    // Přidání třídy pro zvýraznění
+    novyElement.classList.add("highlighted");
+
+    // Přidání event listeneru pro odstranění elementu po kliknutí
+    novyElement.addEventListener("click", function () {
+        container.removeChild(novyElement);
+    });
+}
+
+button.addEventListener("click", funkcePoKliknuti);
+
+// Funkce pro generování náhodné barvy
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
