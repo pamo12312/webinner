d9dfba08973c282e18c484b62721ab57fe082f7f
Patrik <patri.moravec@email.cz>
2023-12-08 08:22:11 +0100

    inner text v js

---
+++ js.js
@@ -0,0 +1,16 @@
+function displayInnerText() {
+    let resultElement = document.querySelector('#result');
+    let testElement = document.querySelector('#testElement');
+    resultElement.innerText = 'innerText: ' + testElement.innerText;
+}
+
+// Function to get and display textContent using querySelector
+function displayTextContent() {
+    let resultElement = document.querySelector('#result');
+    let testElement = document.querySelector('#testElement');
+    resultElement.textContent = 'textContent: ' + testElement.textContent;
+}
+
+// Run both functions
+displayInnerText();
+displayTextContent();
